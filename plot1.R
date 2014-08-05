a52739be57fe4e549bdbb905cae4cf9abf3e940c
David Parker <davparker@gmail.com>
2014-08-05 05:18:46 -0500

    completed first three plots

---
+++ plot1.R
@@ -0,0 +1,26 @@
+# Author:   David Parker
+# Course:   Exploratory Data Analysis
+# School:   John Hopkins University
+# Provider: Coursera
+# Project:  plot1.R
+
+# clear environment
+rm(list=ls())
+
+# read power data
+dfpower <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?",
+                      colClasses = c("character", "character", "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", "numeric"),  stringsAsFactors = FALSE)
+
+# use transform to create a new DateTime field while dropping individual Date & Time fields
+dfpower <- transform(dfpower, DateTime = strptime(paste(Date, Time),  "%d/%m/%Y %H:%M:%S"), stringsAsFactors = 0)[, c(10, 3:9)]
+
+# subset results to days 2007-02-01, 2007-02-02
+dfpowsub <- dfpower[which(dfpower$DateTime >= "2007-02-01" & dfpower$DateTime < "2007-02-03"), ]
+
+# Plot1 | produce plot1 and copy to plot1.png
+hist(dfpowsub$Global_active_power, col = "red",
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)" )
+dev.copy(png, "plot1.png")
+dev.off()
+
---
+++ plot2.R
@@ -0,0 +1,27 @@
+# Author:   David Parker
+# Course:   Exploratory Data Analysis
+# School:   John Hopkins University
+# Provider: Coursera
+# Project:  plot2.R
+
+# clear environment
+rm(list=ls())
+
+# read power data
+dfpower <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?",
+                      colClasses = c("character", "character", "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", "numeric"),  stringsAsFactors = FALSE)
+
+# use transform to create a new DateTime field while dropping individual Date & Time fields
+dfpower <- transform(dfpower, DateTime = strptime(paste(Date, Time),  "%d/%m/%Y %H:%M:%S"), stringsAsFactors = 0)[, c(10, 3:9)]
+
+# subset results to days 2007-02-01, 2007-02-02
+dfpowsub <- dfpower[which(dfpower$DateTime >= "2007-02-01" & dfpower$DateTime < "2007-02-03"), ]
+
+# Plot2 | produce plot2 and copy to plot2.png
+with(dfpowsub, plot(DateTime, Global_active_power, type = "l",
+                    ylab = "Global Active Power (kilowatts)") )
+dev.copy(png, "plot2.png")
+dev.off()
+
+
