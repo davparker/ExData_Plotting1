5f1314fceaad752d98d7d9fae31e6eb07382a21e
David Parker <davparker@gmail.com>
2014-08-05 05:53:28 -0500

    adjusted label sizes plot1.R

--- plot1.R
+++ plot1.R
@@ -19,7 +19,8 @@ dfpower <- transform(dfpower, DateTime = strptime(paste(Date, Time),  "%d/%m/%Y 
 dfpowsub <- dfpower[which(dfpower$DateTime >= "2007-02-01" & dfpower$DateTime < "2007-02-03"), ]
 
 # Plot1 | produce plot1 and copy to plot1.png
-hist(dfpowsub$Global_active_power, col = "red",
+par(mar = c(4, 4, 2, 2), oma = c(1, 1, 1, 1))
+hist(dfpowsub$Global_active_power, col = "red", cex.main = 0.9, cex.lab = 0.75, cex.axis = 0.75,
      main = "Global Active Power", xlab = "Global Active Power (kilowatts)" )
 dev.copy(png, "plot1.png")
 dev.off()
