57252b51e494dedd92b65f3f761cd3d2fcc8abfe
David Parker <davparker@gmail.com>
2014-08-05 17:22:56 -0500

    switched from dev.copy() to png() to create plot3.png. Corrected format of legend

--- plot3.R
+++ plot3.R
@@ -19,7 +19,7 @@ dfpower <- transform(dfpower, DateTime = strptime(paste(Date, Time),  "%d/%m/%Y 
 dfpowsub <- dfpower[which(dfpower$DateTime >= "2007-02-01" & dfpower$DateTime < "2007-02-03"), ]
 
 # Plot3 | produce plot3 and copy to plot3.png
-
+# plot to screen
 par(mar = c(3, 4, 2, 0), oma = c(1, 0, 1, 0))
 with(dfpowsub, plot(DateTime, Sub_metering_1, type = "l", col = "black", 
                     xlab = "", ylab ="Energy sub metering", cex.axis = 0.75, cex.lab = .75))
@@ -29,5 +29,16 @@ par(font = 1)
 legend("topright", col = c("black", "red", "blue"), cex = 0.7,
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty=c(1,1,1))
-dev.copy(png, "plot3.png")
+
+# plot to file
+png(file = "plot3.png", bg = "transparent")
+par(bg = NA)
+with(dfpowsub, plot(DateTime, Sub_metering_1, type = "l", col = "black", 
+                    xlab = "", ylab ="Energy sub metering",))
+with(dfpowsub, lines(DateTime, Sub_metering_2, type = "l", col = "red"))
+with(dfpowsub, lines(DateTime, Sub_metering_3, type = "l", col = "blue"))
+par(font = 1)
+legend("topright", col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=c(1,1,1))
 dev.off()
